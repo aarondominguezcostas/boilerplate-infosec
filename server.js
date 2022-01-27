ca7fe0a05602b1a971c8349516e294e4054bddda
adominguez <a.dominguez@tuinet.net>
2022-01-27 13:49:16 +0100

    Added bcrypt

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var express = require('express');
 var app = express();
 var fs = require('fs');
 var path = require('path');
-const bcrypt = require('bcrypt');
+let bcrypt = require('bcrypt');
 
 app.use(function(req, res, next) {
   res.set({
